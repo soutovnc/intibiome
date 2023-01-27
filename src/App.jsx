5cfe3a623465151edf53d81bb17a4d47510470d4
Vinicius <viniciusdesouto@gmail.com>
2023-01-27 18:12:17 -0300

    add styles with fonts, colors and initial values that I'll apply in global aplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 
 export default function App() {
 
   return (
-    <h1>let's get start!</h1>
+    <ThemeProvider theme={defaultTheme}>
+
+      <h1>let's get start!</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
---
+++ src/styles/themes/default.js
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+  'white': '#FFFFFF',
+
+  'blue-300': '#389CD6',
+  'blue-400': '#549BD1',
+
+  'gray-300': '#D9D5D5',
+  'gray-400': '#9D8D8D',
+  'gray-500': '#9D8E8E',
+  'gray-600': '#7B7070',
+
+  'red': '#EE6381',
+  'green': '#309D5F',
+}
